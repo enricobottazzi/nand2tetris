aedf13e82ec6b98c856f317c92d536e9d3016927
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2024-04-08 17:53:09 +0200

    feat: add `Fill.asm`

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,69 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP1)
+    @24576
+    D=A
+    @max
+    M=D
+
+    @16384
+    D=A
+    @i
+    M=D
+
+    // Wait for a key press
+    @KBD
+    D=M
+    @SETWHITE
+    D;JEQ // If no key is pressed, go to SETWHITE
+
+    // Set the screen to black
+
+    (LOOP3)
+        @i
+        D=M
+        @max
+        D=D-M
+        @LOOP1
+        D;JEQ // If i = max, go to LOOP1
+
+        // Else set the screen to black
+        @i 
+        A=M
+        M=-1
+
+        // Increment i
+        @i
+        M=M+1
+
+        @LOOP3
+        0;JMP
+
+
+(SETWHITE)
+    (LOOP2)
+        @i
+        D=M
+        @max
+        D=D-M
+        @LOOP1
+        D;JEQ // If i = max, go to LOOP1
+
+        // Else set the screen to white
+        @i 
+        A=M
+        M=0
+
+        // Increment i
+        @i
+        M=M+1
+
+        @LOOP2
+        0;JMP
+
+    
+
+
+
+
