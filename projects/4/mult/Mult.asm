068a28e5208ab6c851fa88a91c2da2878a90824e
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2024-04-08 17:27:32 +0200

    feat: add`Mult.asm`

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+   @i // init i = 0
+   M=0
+
+   @sum // init sum = 0
+   M=0
+
+   (LOOP)
+   @i
+   D=M 
+   @R1
+   D=D-M // R1 - i
+   @ASSIGN
+   D;JEQ // if i == R1, goto ASSIGN
+   
+   // else assign sum += R0
+   @R0
+   D=M
+   @sum // load sum
+   M=M+D // sum = sum + R0
+
+   // increment i
+   @i
+   M=M+1
+
+   @LOOP
+   0;JMP // goto LOOP
+
+   (ASSIGN)
+   @sum 
+   D=M
+   @R2
+   M=D // R2 = sum
+
+   // GO TO END
+   @END
+   0;JMP
+
+   (END)
+   @END
+   0;JMP // infinite loop
+
